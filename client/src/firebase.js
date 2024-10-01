20d0ab34b06467d91c6cc4296ad6c9adc9444924
Jihed Mhamdi <jihed.mhamdi2019@gmail.com>
2024-10-01 08:35:14 +0100

    Update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,9 +6,9 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
+  authDomain: 'tunisian-estate.firebaseapp.com',
+  projectId: 'tunisian-estate',
+  storageBucket: 'tunisian-estate.appspot.com',
   messagingSenderId: '1078482850952',
   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
 };
